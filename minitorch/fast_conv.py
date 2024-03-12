67abb9662315bfe612b622781974e69f2c7fa0ff
shreyasriram4 <shreyasriram4@gmail.com>
2024-03-12 18:22:45 -0400

    copy files

---
+++ minitorch/fast_conv.py
@@ -0,0 +1,333 @@
+from typing import Tuple
+
+import numpy as np
+from numba import njit, prange
+
+from .autodiff import Context
+from .tensor import Tensor
+from .tensor_data import (
+    Shape,
+    Strides,
+    broadcast_index,
+    index_to_position,
+    to_index,
+)
+from .tensor_functions import Function
+
+# This code will JIT compile fast versions your tensor_data functions.
+# If you get an error, read the docs for NUMBA as to what is allowed
+# in these functions.
+to_index = njit(inline="always")(to_index)
+index_to_position = njit(inline="always")(index_to_position)
+broadcast_index = njit(inline="always")(broadcast_index)
+
+
+def _tensor_conv1d(
+    out: Tensor,
+    out_shape: Shape,
+    out_strides: Strides,
+    out_size: int,
+    input: Tensor,
+    input_shape: Shape,
+    input_strides: Strides,
+    weight: Tensor,
+    weight_shape: Shape,
+    weight_strides: Strides,
+    reverse: bool,
+) -> None:
+    """
+    1D Convolution implementation.
+
+    Given input tensor of
+
+       `batch, in_channels, width`
+
+    and weight tensor
+
+       `out_channels, in_channels, k_width`
+
+    Computes padded output of
+
+       `batch, out_channels, width`
+
+    `reverse` decides if weight is anchored left (False) or right.
+    (See diagrams)
+
+    Args:
+        out (Storage): storage for `out` tensor.
+        out_shape (Shape): shape for `out` tensor.
+        out_strides (Strides): strides for `out` tensor.
+        out_size (int): size of the `out` tensor.
+        input (Storage): storage for `input` tensor.
+        input_shape (Shape): shape for `input` tensor.
+        input_strides (Strides): strides for `input` tensor.
+        weight (Storage): storage for `input` tensor.
+        weight_shape (Shape): shape for `input` tensor.
+        weight_strides (Strides): strides for `input` tensor.
+        reverse (bool): anchor weight at left or right
+    """
+    batch_, out_channels, out_width = out_shape
+    batch, in_channels, width = input_shape
+    out_channels_, in_channels_, kw = weight_shape
+
+    assert (
+        batch == batch_
+        and in_channels == in_channels_
+        and out_channels == out_channels_
+    )
+    s1 = input_strides
+    s2 = weight_strides
+
+    for batch_idx in prange(batch_):
+        for out_channel_idx in prange(out_channels_):
+            for out_width_idx in prange(out_width):
+                total_acc = 0.0
+                for in_channel_idx in prange(in_channels):
+                    for kernel_width_idx in prange(kw):
+                        pos_w = (
+                            out_channel_idx * s2[0]
+                            + in_channel_idx * s2[1]
+                            + kernel_width_idx * s2[2]
+                        )
+                        input_index = np.zeros(3)
+                        if not reverse:
+                            # get input position using strides
+                            pos_in = (
+                                batch_idx * s1[0]
+                                + in_channel_idx * s1[1]
+                                + out_width_idx * s1[2]
+                                + kernel_width_idx * s1[2]
+                            )
+                            val = out_width_idx + kernel_width_idx
+                        else:
+                            pos_in = (
+                                batch_idx * s1[0]
+                                + in_channel_idx * s1[1]
+                                + out_width_idx * s1[2]
+                                - kernel_width_idx * s1[2]
+                            )
+                            val = out_width_idx - kernel_width_idx
+
+                        input_index[0] = batch_idx
+                        input_index[1] = in_channel_idx
+                        input_index[2] = val
+
+                        if 0 <= input_index[2] < width and not reverse:
+                            total_acc += input[pos_in] * weight[pos_w]
+                        elif 0 <= input_index[2] < width and reverse:
+                            total_acc += input[pos_in] * weight[pos_w]
+                out_pos = (
+                    batch_idx * out_strides[0]
+                    + out_channel_idx * out_strides[1]
+                    + out_width_idx * out_strides[2]
+                )
+                out[out_pos] = total_acc
+
+
+tensor_conv1d = njit(parallel=True)(_tensor_conv1d)
+
+
+class Conv1dFun(Function):
+    @staticmethod
+    def forward(ctx: Context, input: Tensor, weight: Tensor) -> Tensor:
+        """
+        Compute a 1D Convolution
+
+        Args:
+            ctx : Context
+            input : batch x in_channel x h x w
+            weight : out_channel x in_channel x kh x kw
+
+        Returns:
+            batch x out_channel x h x w
+        """
+        ctx.save_for_backward(input, weight)
+        batch, in_channels, w = input.shape
+        out_channels, in_channels2, kw = weight.shape
+        assert in_channels == in_channels2
+
+        # Run convolution
+        output = input.zeros((batch, out_channels, w))
+        tensor_conv1d(
+            *output.tuple(), output.size, *input.tuple(), *weight.tuple(), False
+        )
+        return output
+
+    @staticmethod
+    def backward(ctx: Context, grad_output: Tensor) -> Tuple[Tensor, Tensor]:
+        input, weight = ctx.saved_values
+        batch, in_channels, w = input.shape
+        out_channels, in_channels, kw = weight.shape
+        grad_weight = grad_output.zeros((in_channels, out_channels, kw))
+        new_input = input.permute(1, 0, 2)
+        new_grad_output = grad_output.permute(1, 0, 2)
+        tensor_conv1d(
+            *grad_weight.tuple(),
+            grad_weight.size,
+            *new_input.tuple(),
+            *new_grad_output.tuple(),
+            False,
+        )
+        grad_weight = grad_weight.permute(1, 0, 2)
+
+        grad_input = input.zeros((batch, in_channels, w))
+        new_weight = weight.permute(1, 0, 2)
+        tensor_conv1d(
+            *grad_input.tuple(),
+            grad_input.size,
+            *grad_output.tuple(),
+            *new_weight.tuple(),
+            True,
+        )
+        return grad_input, grad_weight
+
+
+conv1d = Conv1dFun.apply
+
+
+def _tensor_conv2d(
+    out: Tensor,
+    out_shape: Shape,
+    out_strides: Strides,
+    out_size: int,
+    input: Tensor,
+    input_shape: Shape,
+    input_strides: Strides,
+    weight: Tensor,
+    weight_shape: Shape,
+    weight_strides: Strides,
+    reverse: bool,
+) -> None:
+    """
+    2D Convolution implementation.
+
+    Given input tensor of
+
+       `batch, in_channels, height, width`
+
+    and weight tensor
+
+       `out_channels, in_channels, k_height, k_width`
+
+    Computes padded output of
+
+       `batch, out_channels, height, width`
+
+    `Reverse` decides if weight is anchored top-left (False) or bottom-right.
+    (See diagrams)
+
+
+    Args:
+        out (Storage): storage for `out` tensor.
+        out_shape (Shape): shape for `out` tensor.
+        out_strides (Strides): strides for `out` tensor.
+        out_size (int): size of the `out` tensor.
+        input (Storage): storage for `input` tensor.
+        input_shape (Shape): shape for `input` tensor.
+        input_strides (Strides): strides for `input` tensor.
+        weight (Storage): storage for `input` tensor.
+        weight_shape (Shape): shape for `input` tensor.
+        weight_strides (Strides): strides for `input` tensor.
+        reverse (bool): anchor weight at top-left or bottom-right
+    """
+    batch_, out_channels, _, _ = out_shape
+    batch, in_channels, height, width = input_shape
+    out_channels_, in_channels_, kh, kw = weight_shape
+
+    assert (
+        batch == batch_
+        and in_channels == in_channels_
+        and out_channels == out_channels_
+    )
+
+    s1 = input_strides
+    s2 = weight_strides
+    # inners
+
+    for i in prange(out_size):
+        out_index = out_shape.copy()
+        to_index(i, out_shape, out_index)
+        my_batch, my_channel, my_height, my_width = out_index
+
+        result = 0.0
+        for c in range(in_channels):
+            for j_h in range(kh):
+                for j_w in range(kw):
+                    pos_idx = np.array([my_channel, c, j_h, j_w], dtype=np.int32)
+                    weight_pos = index_to_position(pos_idx, s2)
+                    input_h = my_height - j_h if reverse else my_height + j_h
+                    input_w = my_width - j_w if reverse else my_width + j_w
+                    if (
+                        input_h >= 0
+                        and input_h < height
+                        and input_w >= 0
+                        and input_w < width
+                    ):
+                        in_pos_idx = np.array([my_batch, c, input_h, input_w], dtype=np.int32)
+                        input_pos = index_to_position(
+                            in_pos_idx, s1
+                        )
+                        result += input[input_pos] * weight[weight_pos]
+
+        out_pos = index_to_position(out_index, out_strides)
+        out[out_pos] = result
+
+
+tensor_conv2d = njit(parallel=True, fastmath=True)(_tensor_conv2d)
+
+
+class Conv2dFun(Function):
+    @staticmethod
+    def forward(ctx: Context, input: Tensor, weight: Tensor) -> Tensor:
+        """
+        Compute a 2D Convolution
+
+        Args:
+            ctx : Context
+            input : batch x in_channel x h x w
+            weight  : out_channel x in_channel x kh x kw
+
+        Returns:
+            (:class:`Tensor`) : batch x out_channel x h x w
+        """
+        ctx.save_for_backward(input, weight)
+        batch, in_channels, h, w = input.shape
+        out_channels, in_channels2, kh, kw = weight.shape
+        assert in_channels == in_channels2
+        output = input.zeros((batch, out_channels, h, w))
+        tensor_conv2d(
+            *output.tuple(), output.size, *input.tuple(), *weight.tuple(), False
+        )
+        return output
+
+    @staticmethod
+    def backward(ctx: Context, grad_output: Tensor) -> Tuple[Tensor, Tensor]:
+        input, weight = ctx.saved_values
+        batch, in_channels, h, w = input.shape
+        out_channels, in_channels, kh, kw = weight.shape
+
+        grad_weight = grad_output.zeros((in_channels, out_channels, kh, kw))
+        new_input = input.permute(1, 0, 2, 3)
+        new_grad_output = grad_output.permute(1, 0, 2, 3)
+        tensor_conv2d(
+            *grad_weight.tuple(),
+            grad_weight.size,
+            *new_input.tuple(),
+            *new_grad_output.tuple(),
+            False,
+        )
+        grad_weight = grad_weight.permute(1, 0, 2, 3)
+
+        grad_input = input.zeros((batch, in_channels, h, w))
+        new_weight = weight.permute(1, 0, 2, 3)
+        tensor_conv2d(
+            *grad_input.tuple(),
+            grad_input.size,
+            *grad_output.tuple(),
+            *new_weight.tuple(),
+            True,
+        )
+        return grad_input, grad_weight
+
+
+conv2d = Conv2dFun.apply
---
+++ minitorch/nn.py
@@ -0,0 +1,174 @@
+from typing import Tuple
+
+from . import operators
+from .autodiff import Context
+from .fast_ops import FastOps
+from .tensor import Tensor
+from .tensor_functions import Function, rand
+
+
+def tile(input: Tensor, kernel: Tuple[int, int]) -> Tuple[Tensor, int, int]:
+    """
+    Reshape an image tensor for 2D pooling
+
+    Args:
+        input: batch x channel x height x width
+        kernel: height x width of pooling
+
+    Returns:
+        Tensor of size batch x channel x new_height x new_width x (kernel_height * kernel_width) as well as the new_height and new_width value.
+    """
+
+    batch, channel, height, width = input.shape
+    kh, kw = kernel
+    assert height % kh == 0
+    assert width % kw == 0
+    new_height = height // kh
+    new_width = width // kw
+    output = (
+        input.contiguous()
+        .view(batch, channel, new_height, kh, new_width, kw)
+        .permute(0, 1, 2, 4, 3, 5)
+    )
+    output = output.contiguous().view(batch, channel, new_height, new_width, kh * kw)
+
+    return (output, new_height, new_width)
+
+
+def avgpool2d(input: Tensor, kernel: Tuple[int, int]) -> Tensor:
+    """
+    Tiled average pooling 2D
+
+    Args:
+        input : batch x channel x height x width
+        kernel : height x width of pooling
+
+    Returns:
+        Pooled tensor
+    """
+    batch, channel, height, width = input.shape
+    new_tile, new_height, new_width = tile(input, kernel)
+    pooled = new_tile.mean(dim=4).view(batch, channel, new_height, new_width)
+
+    return pooled
+
+
+max_reduce = FastOps.reduce(operators.max, -1e9)
+
+
+def argmax(input: Tensor, dim: int) -> Tensor:
+    """
+    Compute the argmax as a 1-hot tensor.
+
+    Args:
+        input : input tensor
+        dim : dimension to apply argmax
+
+
+    Returns:
+        :class:`Tensor` : tensor with 1 on highest cell in dim, 0 otherwise
+
+    """
+    out = max_reduce(input, dim)
+    return out == input
+
+
+class Max(Function):
+    @staticmethod
+    def forward(ctx: Context, input: Tensor, dim: Tensor) -> Tensor:
+        "Forward of max should be max reduction"
+        # TODO: Implement for Task 4.4.
+        out = max_reduce(input, int(dim.item()))
+        ctx.save_for_backward(input, dim)
+        return out
+
+    @staticmethod
+    def backward(ctx: Context, grad_output: Tensor) -> Tuple[Tensor, float]:
+        "Backward of max should be argmax (see above)"
+        # TODO: Implement for Task 4.4.
+        input, dim = ctx.saved_values
+        return (argmax(input, int(dim.item())) * grad_output, dim)
+
+
+def max(input: Tensor, dim: int) -> Tensor:
+    return Max.apply(input, input._ensure_tensor(dim))
+
+
+def softmax(input: Tensor, dim: int) -> Tensor:
+    r"""
+    Compute the softmax as a tensor.
+
+
+    $z_i = \frac{e^{x_i}}{\sum_i e^{x_i}}$
+
+    Args:
+        input : input tensor
+        dim : dimension to apply softmax
+
+    Returns:
+        softmax tensor
+    """
+    out = input.exp()
+    sum_out = out.sum(dim=dim)
+    return out / sum_out
+
+
+def logsoftmax(input: Tensor, dim: int) -> Tensor:
+    r"""
+    Compute the log of the softmax as a tensor.
+
+    $z_i = x_i - \log \sum_i e^{x_i}$
+
+    See https://en.wikipedia.org/wiki/LogSumExp#log-sum-exp_trick_for_log-domain_calculations
+
+    Args:
+        input : input tensor
+        dim : dimension to apply log-softmax
+
+    Returns:
+         log of softmax tensor
+    """
+    # TODO: Implement for Task 4.4.
+    out_exp = input.exp()
+    out_sum = out_exp.sum(dim=dim)
+    out_log = out_sum.log()
+    return input - out_log
+
+
+def maxpool2d(input: Tensor, kernel: Tuple[int, int]) -> Tensor:
+    """
+    Tiled max pooling 2D
+
+    Args:
+        input: batch x channel x height x width
+        kernel: height x width of pooling
+
+    Returns:
+        Tensor : pooled tensor
+    """
+    batch, channel, height, width = input.shape
+    new_tile, new_height, new_width = tile(input, kernel)
+    pooled = max(new_tile, 4).view(batch, channel, new_height, new_width)
+
+    return pooled
+
+
+def dropout(input: Tensor, rate: float, ignore: bool = False) -> Tensor:
+    """
+    Dropout positions based on random noise.
+
+    Args:
+        input : input tensor
+        rate : probability [0, 1) of dropping out each position
+        ignore : skip dropout, i.e. do nothing at all
+
+    Returns:
+        tensor with randoom positions dropped out
+    """
+    if ignore:
+        return input
+    else:
+        input_shape = input.shape
+        rate_vals = rand(input_shape)
+        bool_vals = rate_vals > rate
+        return bool_vals * input
